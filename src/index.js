4b475cde03f4c034ef6371f0f23a66eb0e5b2110
Cristofer <c.valdovino.v@gmail.com>
2022-07-13 17:44:25 -0400

    ADD center game

--- src/index.js
+++ src/index.js
@@ -1,41 +1,44 @@
-import Phaser, { Scene } from "phaser";
-import MenuScene from "./scenes/MenuScene";
-import PauseScene from "./scenes/PauseScene";
-import PlayScene from "./scenes/PlayScene";
-import PreloadScene from "./scenes/PreloadScene";
-import ScoreScene from "./scenes/ScoreScene";
+import Phaser, { Scene } from 'phaser'
+import MenuScene from './scenes/MenuScene'
+import PauseScene from './scenes/PauseScene'
+import PlayScene from './scenes/PlayScene'
+import PreloadScene from './scenes/PreloadScene'
+import ScoreScene from './scenes/ScoreScene'
 
-const WIDHT = 400;
-const HEIGHT = 600;
+const WIDHT = 400
+const HEIGHT = 600
 const BIRD_POSITION = {
   x: WIDHT * 0.1,
   y: HEIGHT / 2,
-};
+}
 const SHARED_CONFIG = {
   width: WIDHT,
   height: HEIGHT,
   startPosition: BIRD_POSITION,
-};
+}
 
-const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene];
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene]
 
-const createScene = (Scene) => new Scene(SHARED_CONFIG);
+const createScene = (Scene) => new Scene(SHARED_CONFIG)
 
-const initScene = () => Scenes.map(createScene);
+const initScene = () => Scenes.map(createScene)
 
 const config = {
   type: Phaser.AUTO,
-  mode: Phaser.Scale.FIT,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   ...SHARED_CONFIG,
   pixelArt: true,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
-    //   debug: true,
+      //   debug: true,
     },
   },
-  parent: "game",
+  parent: 'game',
   scene: initScene(),
-};
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
